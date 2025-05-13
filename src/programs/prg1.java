1e13e80753e25f489cfd7f9a30b9e8836b891b45
Aparna9820 <aparnayedubati9820@gmail.com>
2025-05-13 22:47:11 +0530

    test prg2

--- src/programs/prg1.java
+++ src/programs/prg1.java
@@ -2,8 +2,8 @@ package programs;
 
 public class prg1 {
     public static void main(String[] args) {
-        int a=45;
-        a=a+45;
-        System.out.println(a);
+        int _a=45;
+        _a=_a+45;
+        System.out.println(_a);
     }
 }
---
+++ src/programs/prg2.java
@@ -0,0 +1,24 @@
+package programs;
+
+public class prg2 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        System.out.println(a);
+
+        main(20);
+
+    }
+
+    public  static void main ( int a ){
+
+        a = 10;
+
+        System.out.println(a);
+
+    }
+
+}
+
