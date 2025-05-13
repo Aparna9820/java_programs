687156264c53386dc4be45cbd581e53b52319760
Aparna9820 <aparnayedubati9820@gmail.com>
2025-05-13 22:33:11 +0530

    test prg1

--- src/programs/HELLOWORLD.java
+++ src/programs/HELLOWORLD.java
@@ -1,6 +1,7 @@
 package programs;
 
-public class HELLOWORLD {
+public class
+HELLOWORLD {
     public static void main(String[] args) {
         System.out.println("hello Aparna!");
     }
---
+++ src/programs/prg1.java
@@ -0,0 +1,9 @@
+package programs;
+
+public class prg1 {
+    public static void main(String[] args) {
+        int a=45;
+        a=a+45;
+        System.out.println(a);
+    }
+}
